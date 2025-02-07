6a217b9bd9e5476088c4be8a1627d39075649abb
its-Anmol-Sharma <144224825+its-Anmol-Sharma@users.noreply.github.com>
2025-02-08 01:20:55 +0530

    Minor bug update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { About } from './components/About/About'
 import { Experience } from './components/Experience/Experience'
 import { Hero } from './components/Hero/Hero'
 import { Navbar } from './components/Navbar/Navbar'
-import { Projects } from './components/projects/Projects'
+import { Projects } from './components/Projects/Projects'
 import { Contact} from './components/Contact/contact'
 // import Card from './components/Projects/CardProject'
 
